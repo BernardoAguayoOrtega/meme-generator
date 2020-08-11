9488b73bb242838b941a45406d778a0297dd7823
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-11 18:21:00 -0500

    finish basic structure react app

---
+++ app/public/src/components/App.jsx
@@ -0,0 +1,20 @@
+//import react
+import React, { Component } from "react";
+
+//app component
+export default class App extends Component {
+  //constructor
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  //render method
+  render() {
+    return (
+      <section>
+        <h1>Hello there</h1>
+      </section>
+    );
+  }
+}
---
+++ app/public/src/components/index.jsx
@@ -0,0 +1,12 @@
+//import react
+import React from "react";
+import ReactDom from "react-dom";
+
+//import app
+import App from "./App.jsx";
+
+//const for root
+const root = document.getElementById("root");
+
+//dom render
+ReactDom.render(<App />, root);
